059747de61d560b772476b1301ff4dcb889ff10e
Hilton Rocha <hilton-bruno-santos@hotmail.com>
2017-04-20 15:28:32 -0300

    Webmotors

--- features/step_definitions/busca_carro_steps.rb
+++ features/step_definitions/busca_carro_steps.rb
@@ -7,10 +7,17 @@ Dado(/^que acesse a página de busca por veiculo$/) do
   #touch(abamoto,timeout:10)
 end
 
-Quando(/^faço uma busca por "([^"]*)", "([^"]*)", "([^"]*)"$/) do |marca, arg2, arg3|
+Quando(/^faço uma busca por "([^"]*)", "([^"]*)", "([^"]*)"$/) do |marca, modelo, versao|
   tap_when_element_exists("* id:'search_title'{text CONTAINS 'Marca'}",timeout:140)
   tap_when_element_exists("* id:'name_text'{text CONTAINS #{marca}}",timeout:100, timeoutmessage:"TIMEOUT ESTORADO")
-
+  tap_when_element_exists("* id:'search_title' index:1")
+  enter_text("* id:'search_src_text'", modelo)
+  system("adb shell input keyevent KEYCODE_ENTER") 
+  tap_when_element_exists("* android.widget.TextView{text CONTAINS #{modelo}}",timeout:140)
+  tap_when_element_exists("* id:'search_title' index:3", timeout:10)
+  tap_when_element_exists("* id:'activity_single_selection_row_title'{text CONTAINS[c] '#{versao}'}", timeout:10)
+  touch("* id:'search_form_confirm'")
+  sleep 5
 end
 
 Então(/^deve ser exibido os veiculos referentes a minha busca$/) do
